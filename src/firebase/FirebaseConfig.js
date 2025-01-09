c4fef7d0657254501c2f22b55e16fdbbbd68019c
Andy Vu <andyvu2026@u.northwestern.edu>
2025-01-08 20:41:06 -0600

    Added Firebase config

---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDs962Jh1sH_fkkOtdf2FNlYyomF-4n_F8",
+    authDomain: "furniture-flow.firebaseapp.com",
+    projectId: "furniture-flow",
+    storageBucket: "furniture-flow.firebasestorage.app",
+    messagingSenderId: "682821688739",
+    appId: "1:682821688739:web:8e7d11625aa74142d7ce16",
+    measurementId: "G-CN19TPW0RB"
+  };
