36b529b257de6f7b9969bea68222bca770a0022f
MusicTastingClubNU <musictastingclub@gmail.com>
2025-01-12 15:54:51 -0600

    NO LISTINGS IN THIS AREA

--- src/components/ItemPanel.jsx
+++ src/components/ItemPanel.jsx
@@ -8,7 +8,7 @@ function ItemPanel({ items }) {
   for (let i = 0; i < items.length; i += cardsPerRow) {
     rows.push(items.slice(i, i + cardsPerRow));
   }
-  
+
   return (
     <div className="bg-white overflow-x-auto overflow-y-auto max-h-[80vh]">
       <div className="flex flex-col gap-4 p-4">
@@ -16,25 +16,37 @@ function ItemPanel({ items }) {
           <div key={rowIndex} className="flex flex-row gap-4">
             {row.map((item, itemIndex) => (
               <MediaCard
-              name={item.name}
-              price={new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-              }).format(Number(item.price))}
-              user="dr.riesbeck"
-              description={item.description}
-              className="flex-1"
-            />
+                name={item.name}
+                price={new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(Number(item.price))}
+                user="dr.riesbeck"
+                description={item.description}
+                className="flex-1"
+              />
             ))}
             {}
             {row.length < cardsPerRow &&
               Array(cardsPerRow - row.length)
                 .fill(null)
                 .map((_, emptyIndex) => (
-                  <div key={`empty-${rowIndex}-${emptyIndex}`} className="flex-1" />
+                  <div
+                    key={`empty-${rowIndex}-${emptyIndex}`}
+                    className="flex-1"
+                  />
                 ))}
           </div>
         ))}
+        {rows.length == 0 ? (
+          <div className="flex items-center justify-center h-full">
+            <h1 className="text-black text-4xl font-bold text-center">
+              NO LISTINGS IN THIS AREA
+            </h1>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
