dbc10dae73348744d28e88957df57f9e774b550d
Andy Vu <andyvu2026@u.northwestern.edu>
2025-01-12 14:04:01 -0600

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,14 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Map from "./components/Map";
+import SearchBar from "./components/SearchBar"
+import { Grid } from "@mui/material";
+import Navigation from "./components/Navigation"
+import Item from "./components/Item"
 
 const App = () => {
   return (
     <>
-      {/* Full-width SearchBar */}
-      <div>
-        <SearchBar />
-      </div>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>
-            <Map />
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-            <ItemPanel />
-          </Item>
-        </Grid>
-      </Grid>
-
       <div>
         <Navigation />
       </div>
