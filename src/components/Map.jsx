0ffafab41e2f869982dea93f24df1420b609afb7
Andy Vu <andyvu2026@u.northwestern.edu>
2025-01-12 13:39:43 -0600

    fetch items within bounds

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,7 +6,8 @@ import {
   LoadScript,
   InfoBoxF,
 } from '@react-google-maps/api';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const containerStyle = {
@@ -61,9 +62,45 @@ image: "https://i.scdn.co/image/ab67616d0000b273dc30583ba717007b00cceb25",
 },
 ];
 
+function fetchItems(bounds) {
+    const minLat = bounds.south
+    const maxLat = bounds.north
+    const minLon = bounds.west
+    const maxLon = bounds.east
+
+    axios.post("https://fetchitems-jbhycjd2za-uc.a.run.app", {
+        "minLat": minLat,
+        "maxLat": maxLat,
+        "minLon": minLon,
+        "maxLon": maxLon,
+    }, 
+    {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then((res) => {
+        console.log("res:", res)
+    })
+    .catch((error) => {
+        console.error(error.response.data)
+    })
+}
 
 const Map = () => {
     const [activeMarker, setActiveMarker] = useState(0 | null);
+    const [gMap, setGMap] = useState(null)
+    const [mapBounds, setMapBounds] = useState(null)
+
+    function updateBounds(newBounds) {
+        setMapBounds({
+            north: newBounds.getNorthEast().lat(),
+            east: newBounds.getNorthEast().lng(),
+            south: newBounds.getSouthWest().lat(),
+            west: newBounds.getSouthWest().lng(),
+        });
+    }
+
     const handleActiveMarker = (marker) => { 
         console.log('activemarker changed');
         if (marker === activeMarker) {
@@ -71,11 +108,32 @@ const Map = () => {
         }
         setActiveMarker(marker);
     };
+
     const handleOnLoad = (map) => {
         const bounds = new google.maps.LatLngBounds();
         markers.forEach(({ position }) => bounds.extend(position));
         map.fitBounds(bounds);
+        updateBounds(bounds)
+        setGMap(map)
     };
+
+    const handleZoomChange = () => {
+        if (gMap) {
+            updateBounds(gMap.getBounds())
+        }
+    }
+
+    const handleDragEnd = () => {
+        if (gMap) {
+            updateBounds(gMap.getBounds())
+        }
+    }
+
+    useEffect(() => {
+        if (mapBounds) {
+            fetchItems(mapBounds)
+        }
+    }, [mapBounds])
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
@@ -88,6 +146,8 @@ const Map = () => {
             mapContainerStyle={containerStyle}
             center={center}
             zoom={2}
+            onDragEnd={handleDragEnd}
+            onZoomChanged={handleZoomChange}
         >
             {markers.map(({ id, name, position, image }) => (
                 <MarkerF key={id} position={position} icon={{url:image, scaledSize: new google.maps.Size(65, 65)}} options={{borderRadius:"100%"}} onClick={() => handleActiveMarker(id)}> // Adjust these numbers to change size
