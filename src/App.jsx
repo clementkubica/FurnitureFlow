8228681ab9ac138c787fed3ff7c11a0a2a8b6086
MusicTastingClubNU <musictastingclub@gmail.com>
2025-01-19 17:28:19 -0600

    Making HomePage its own component

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,16 @@ import Map from "./components/Map";
 import ItemPanel from "./components/ItemPanel";
 import Navigation from "./components/Navigation";
 import Login from "./components/Login";
+import HomePage from "./components/HomePage";
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { AuthProvider, useAuth } from "./services/auth"; // Auth Context
-
-const HomePage = () => {
-  const user = useAuth();
-  const [visibleItems, setVisibleItems] = useState([]); // State for items
-  const [mapBounds, setMapBounds] = useState(null); // State for map bounds
-
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return (
-    <div>
-      <Navigation 
-        mapBounds={mapBounds} 
-        setMapBounds={setMapBounds} 
-        visibleItems={visibleItems} 
-        setVisibleItems={setVisibleItems} 
-      />
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 2 }}>
-          <Map 
-            visibleItems={visibleItems} 
-            setVisibleItems={setVisibleItems} 
-            mapBounds={mapBounds} 
-            setMapBounds={setMapBounds} 
-          />
-        </div>
-        <div style={{ flex: 1 }}>
-          <ItemPanel items={visibleItems} />
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const App = () => {
   return (
